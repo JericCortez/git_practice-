e46c9d71e19a5c84ddf486a39164f7e1e934282c
JericCortez <jericayson2@gmail.com>
2024-09-12 01:16:44 +0000

    initial

---
+++ variables.js
@@ -0,0 +1,9 @@
+let name = "Jeric Ayson"
+let whatDoYouWannaBecomeInYourLife = "pprogrammer"
+let gender = "Male"
+let twitterHandlWe = "Jeric C"
+
+console.log("Name:", name);
+console.log("What do you want to do become:", whatDoYouWannaBecomeInLife);
+console.log("Gender :", gender);
+console.log("twitter Handle :", TwitterHandle);
