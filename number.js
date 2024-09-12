77805776bbc363acc4da1b8e13ce6c65552a373b
JericCortez <jericayson2@gmail.com>
2024-09-12 03:15:07 +0000

    initial

---
+++ number.js
@@ -0,0 +1,20 @@
+let firstFavNum = 23; 
+let secondFavNum = 9;
+
+let additionResult = firstFavNum + secondFavNum;
+console.log('Addition:', additionResult);
+
+let subtractionResult = firstFavNum - secondFavNum;
+console.log('Subtraction:', subtractionResult);
+
+let multiplicationResult = firstFavNum * secondFavNum;
+console.log('Multiplication:', multiplicationResult);
+
+let divisionResult = firstFavNum / secondFavNum;
+console.log('Division:', divisionResult);
+
+let modulusResult = firstFavNum % secondFavNum;
+console.log('Modulus:', modulusResult);
+
+let exponentiationResult = firstFavNum ** secondFavNum;
+console.log('Exponentiation (Power):', exponentiationResult);
