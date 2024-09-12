dae845abb5e8ba94891202294e05cd0ca6456808
JericCortez <jericayson2@gmail.com>
2024-09-12 03:34:41 +0000

    initial

---
+++ omparison.js
@@ -0,0 +1,24 @@
+let firstFavNumb = 23;
+let secondFavNumb = 9;
+
+console.log('Greater than:', firstFavNumb > secondFavNumb);
+
+console.log('Less than:', firstFavNumb < secondFavNumb);
+
+console.log('Greater than or equal to:', firstFavNumb >= secondFavNumb);
+
+console.log('Less than or equal to:', firstFavNumb <= secondFavNumb);
+
+console.log('Strict equality (===):', firstFavNumb === secondFavNumb);
+
+console.log('Loose equality (==):', firstFavNumb == secondFavNumb);
+
+console.log('Strict inequality (!==):', firstFavNumb !== secondFavNumb);
+
+console.log('Loose inequality (!=):', firstFavNumb != secondFavNumb);
+
+console.log('AND:', firstFavNumb > 5 && secondFavNumb > 1);
+
+console.log('OR:', firstFavNumb > 5 || secondFavNumb > 5);
+
+console.log('NOT:', !(firstFavNumb > 5));
